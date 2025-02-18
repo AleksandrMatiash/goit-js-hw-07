e3b29fe1238b84cfc7d001dd5d08e59e0eaaece4
Aleksandr <matyash33@gmail.com>
2025-02-18 20:00:05 +0100

    hw-7

---
+++ js/task-6.js
@@ -0,0 +1,12 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const controls = document.querySelector(".controls");
+
+controls.addEventListener("click", fn)
+
+function fn(event) {
+}
